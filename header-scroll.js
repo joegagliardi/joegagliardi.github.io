d11e930a27ae3f356030aae56e3e74d5389dda39
joegagliardi <joegagliardi14@gmail.com>
2023-05-14 14:36:01 -0500

    Header scroll fix

--- header-scroll.js
+++ header-scroll.js
@@ -1,12 +1,15 @@
 const wrapper = document.querySelector(".homepage-wrapper");
+const navbar = document.getElementById("navbar");
 var lastScrollTop = 0;
-navbar = document.getElementById("navbar");
+
 wrapper.addEventListener("scroll", function() {
     var scrollTop = wrapper.scrollTop;
     if (scrollTop > lastScrollTop) {
-        navbar.style.top = "-168px";
+        navbar.classList.add("slide-up");
+        navbar.classList.remove("slide-down");
     } else {
-        navbar.style.top = "0";
+        navbar.classList.add("slide-down");
+        navbar.classList.remove("slide-up");
     }
     lastScrollTop = scrollTop;
 });
