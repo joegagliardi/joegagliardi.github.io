033b77a1c8688c3d2d04d7575d60ba99a2be9ac2
joegagliardi <joegagliardi14@gmail.com>
2023-05-24 20:18:23 -0500

    Attempt to fix scroll on phone

--- header-scroll.js
+++ header-scroll.js
@@ -1,9 +1,8 @@
-const wrapper = document.querySelector(".homepage-wrapper");
 const navbar = document.getElementById("navbar");
-var lastScrollTop = 0;
+let lastScrollTop = 0;
 
-wrapper.addEventListener("scroll", function() {
-    var scrollTop = wrapper.scrollTop;
+function handleScroll() {
+    const scrollTop = window.scrollY || document.documentElement.scrollTop;
     if (scrollTop > lastScrollTop) {
         navbar.classList.add("slide-up");
         navbar.classList.remove("slide-down");
@@ -12,4 +11,15 @@ wrapper.addEventListener("scroll", function() {
         navbar.classList.remove("slide-up");
     }
     lastScrollTop = scrollTop;
-});
+}
+
+function addScrollListener() {
+    if (window.innerWidth <= 480) {
+        window.addEventListener("scroll", handleScroll);
+    } else {
+        window.removeEventListener("scroll", handleScroll);
+    }
+}
+
+window.addEventListener("resize", addScrollListener);
+addScrollListener();
