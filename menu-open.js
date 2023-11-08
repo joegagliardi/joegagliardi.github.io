fb6e5dcb363942fe36192e7a151c88a383d56816
joegagliardi <joegagliardi14@gmail.com>
2023-11-08 13:39:22 -0600

    Close Button

--- menu-open.js
+++ menu-open.js
@@ -2,10 +2,16 @@ const menuButton = document.getElementById("menu-button");
 /*const closeButton = document.getElementById("close-button");*/
 const menu = document.getElementById("menu");
 const headerMenuButton = document.getElementById("menu-button-2");
+const closeMenuButton = document.getElementById("close-button");
 
 menuButton.addEventListener("click", () => {
     menu.classList.toggle("show");
     menuButton.classList.toggle("active");
+});
+
+headerMenuButton.addEventListener("click", () => {
+    menu.classList.toggle("show");
+    headerMenuButton.classList.toggle("active");
 });
 
 headerMenuButton.addEventListener("click", () => {
