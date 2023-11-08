68757e859ca95866a50ef19fe023d616c3bab1d6
joegagliardi <joegagliardi14@gmail.com>
2023-11-07 22:05:25 -0600

    Connect Header Button

--- menu-open.js
+++ menu-open.js
@@ -1,10 +1,16 @@
 const menuButton = document.getElementById("menu-button");
 /*const closeButton = document.getElementById("close-button");*/
 const menu = document.getElementById("menu");
+const headerMenuButton = document.getElementById("menu-button-2");
 
 menuButton.addEventListener("click", () => {
     menu.classList.toggle("show");
     menuButton.classList.toggle("active");
+});
+
+headerMenuButton.addEventListener("click", () => {
+    menu.classList.toggle("show");
+    headerMenuButton.classList.toggle("active");
 });
 
 /*
